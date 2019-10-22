aedf79c211baf8def539b109dc8a7c801b75a128
edward-teixeira <etdj@aluno.ifnmg.edu.br>
2019-10-22 08:12:28 -0300

    Atenticação de usuário adicionado

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 //Dependencias iniciais
-require('./db/connect');
+require('./src/db/connect');
 const express = require('express');
-const cookieParser = require('./middleware/cookieParser');
+const cookieParser = require('./src/middleware/cookieParser');
 const createError = require('http-errors');
 const path = require('path');
-const userController = require('./controller/userController');
-const authMiddleware = require('./middleware/auth');
+const userController = require('./src/controller/userController');
+const authMiddleware = require('./src/middleware/auth');
 
 //Sobe o servidor e abre a conexão com o banco
 const app = express();
 
 //Configuração de middlewares
-app.use(require('./middleware/bodyParser').json());
-app.use(require('./middleware/bodyParser').urlencoded({ extended: true }));
-app.use(require('./middleware/logger'));
+app.use(require('./src/middleware/bodyParser').json());
+app.use(require('./src/middleware/bodyParser').urlencoded({ extended: true }));
+app.use(require('./src/middleware/logger'));
 app.use(cookieParser());
 
 //Home page
-app.use('/', require('./routes/index'));
+app.use('/', require('./src/routes/index'));
 //Create user
 app.post('/user', userController.create_user);
 //Rota para login
-app.use('/sessions', require('./routes/sessionRouter'));
+app.use('/sessions', require('./src/routes/sessionRouter'));
 //Global Middlware
 app.use(authMiddleware.authHeader);
 //Update user
 app.put('/user', userController.update_user);
-app.use('/user/plantacao', require('./routes/plantacaoRouter'));
+app.use('/user/plantacao', require('./src/routes/plantacaoRouter'));
 
 module.exports = app;
